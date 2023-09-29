0f2e7dd3738e154fa2f4aa7e858b9e44be4692be
Johnny Davenport <johnny@pathmind.com>
2023-09-28 17:29:25 -0700

    todos

--- sql_app/main.py
+++ sql_app/main.py
@@ -23,8 +23,10 @@ def get_db():
 @app.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
+        
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+
     return crud.create_user(db=db, user=user)
 
 
@@ -34,19 +36,11 @@ def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return users
 
 
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
 @app.post("/games/new/", response_model=schemas.Game)
-def create_game_for_user(
-    user_id: int, game: schemas.Game, db: Session = Depends(get_db)
-):
-    return crud.create_user_game(db=db, game=game, user_id=user_id)
+def create_game(db: Session = Depends(get_db)):
+    # TODO: determine which user sent the request by JWT bearer
+    db_user = crud.get_user_by_id(db, user_id)
+    return crud.create_user_game(db=db, user_id=user_id)
     
 
 @app.put("/games/invite/{invitee_id}/", response_model=schemas.Game)
